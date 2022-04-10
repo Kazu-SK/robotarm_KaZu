a8202e521418aca04601c8d4428fd4c2dd78a43a
Kazu-SK <robotarm2019@gmail.com>
2022-04-10 15:21:40 +0900

    robot_vision subscriber

--- robot_vision/include/robot_vision/robot_vision.hpp
+++ robot_vision/include/robot_vision/robot_vision.hpp
@@ -2,23 +2,39 @@
 #include <chrono>
 #include <memory>
 
+#include <cstdlib>
 
 #include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/image.hpp"
+//#include <sensor_msgs/image_encodings.h>
+
 #include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <image_transport/image_transport.hpp>
+
+#include <cv_bridge/cv_bridge.h>
+//#include "sensor_msgs/msg/camera_info.hpp"
 
 
-using namespace std::chrono_literals;
-
+using namespace std::placeholders;
 
 
 class RobotVision : public rclcpp::Node{
 
 private:
 
+	rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub;
+
+
 public:
 
-	RobotVision() : Node("RobotVision"){
+	void CallbackImage(const sensor_msgs::msg::Image::SharedPtr msg);
 
+
+	RobotVision() : Node("RobotVision"){
+		image_sub = this->create_subscription<sensor_msgs::msg::Image>("image_raw", 10, std::bind(&RobotVision::CallbackImage, this, _1));
 
 	}
 
--- robot_vision/src/robot_vision.cpp
+++ robot_vision/src/robot_vision.cpp
@@ -1,2 +1,32 @@
 
 #include "robot_vision/robot_vision.hpp"
+
+
+
+void RobotVision::CallbackImage(const sensor_msgs::msg::Image::SharedPtr msg){
+
+	
+	/*
+	cv_bridge::CvImagePtr cv_ptr;
+
+
+	cv_ptr = cv_bridge::toCvCopy(msg, "BGR8");
+	*/
+
+	
+
+
+	/*
+	try{
+
+		cv::imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image);
+	}
+	catch (cv_bridge::Exception& e){
+		
+		
+	}	
+	*/
+
+	//RCLCPP_INFO(this->get_logger(), "height = %d", msg->height);
+
+}
